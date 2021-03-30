e8a88b00889891bab4802892597d9285fa19b6de
wangdabaoqq <614692339@qq.com>
2021-03-30 18:34:46 +0800

    处理固定格式

--- js/poly.js
+++ js/poly.js
@@ -34,7 +34,7 @@
         let newKeys = JSON.parse(polyVal)
         // console.log(JSON.parse(polyVal))
         let newVal = Object.values(newKeys)
-        var flatData = newVal.flat()
+        var flatData = newVal
         let datas = []
         // console.log(newVal.flat())
         // polyVal.split(';').forEach(el => {
@@ -42,18 +42,35 @@
         //     datas.push(transform(typeMap, el.split(',')))
         //   }
         // })
-        flatData.forEach(el => {
-          console.log(el)
-          datas.push(transform(typeMap, [el.longitude, el.latitude]))
-        })
+        // flatData.reduce(el => {
+        //   conso
+        //   datas.push(transform(typeMap, [el.longitude, el.latitude]))
+        // }, [])
+        const getData = () => {
+          return flatData.reduce((init, ele, index) => {
+            // console.log(init, ele)
+            let ddd = ele.map(el => transform(typeMap, [el.longitude, el.latitude]))
+            // init.push(transform(typeMap, [ele.longitude, ele.latitude]))
+              // init[index].push((transform(typeMap, [ele.longitude, ele.latitude]))
+            init[index] = ddd
+            return init
+            // return init
+          }, {})
+        }
         // allkey.forEach(el => {
         //   d[el].forEach(ele => {
         //     datas.push(transform(typeMap, [ele.longitude, ele.latitude]))
         //   })
         // })
-        data.form[1].value = JSON.stringify(datas)
+        let newData = getData()
+        // allkey.forEach(el => {
+        //   d[el].forEach(ele => {
+        //     datas.push(transform(typeMap, [ele.longitude, ele.latitude]))
+        //   })
+        // })
+        // data.form[1].value = JSON.stringify(datas)
         polyline = new AMap.Polyline({
-          path: datas,
+          // path: datas,
           isOutline: true,
           outlineColor: '#ffeeff',
           borderWeight: 3,
@@ -63,17 +80,41 @@
           // 折线样式还支持 'dashed'
           strokeStyle: "solid",
           // strokeStyle是dashed时有效
-          strokeDasharray: [10, 5],
+          // strokeDasharray: [10, 5],
           lineJoin: 'round',
           lineCap: 'round',
           zIndex: 50,
         })
-        // console.log
-        polyline.setMap(window.map)
-        map.setFitView([polyline])
-        // // console.log(r)
-        // console.log(result)
-        // console.log(result.ssplit(','))
+        let aa = Object.values(newData)
+        let getPoly = () => {
+          return aa.map((element) => {
+            console.log(element)
+            let polyline = new AMap.Polyline({
+              path: element,
+              isOutline: true,
+              outlineColor: '#ffeeff',
+              borderWeight: 3,
+              strokeColor: "#3366FF",
+              strokeOpacity: 1,
+              strokeWeight: 6,
+              // 折线样式还支持 'dashed'
+              strokeStyle: "solid",
+              // strokeStyle是dashed时有效
+              // strokeDasharray: [10, 5],
+              lineJoin: 'round',
+              lineCap: 'round',
+              zIndex: 50,
+            })
+            // console.log(init)
+            return polyline
+            // init.push(polyline)
+            // return init
+          })
+        }
+        let polyData = getPoly()
+        var getOverlay = new AMap.OverlayGroup(polyData)
+        window.map.add(getOverlay)
+        window.map.setFitView(polyData)
       } catch (e) {
         layer.msg(e, {
           offset: 't',
